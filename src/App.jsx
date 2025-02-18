7c2155444881ac0169b6c723d105361444bdac98
powiepow <pawtingkasan20@gmail.com>
2025-02-18 20:30:41 +0800

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Hero from './components/hero'
-import './App.css'
-function App(){
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav";
+import Hero from "./components/hero";
+import About from "./components/about";
+import "../src/App";
 
+function App() {
   return (
-    <>
-     <Hero/>
-    </>
-  )
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,27 +1,49 @@
-import '../style/hero.css';
-import reactLogo from "../assets/paw.jpg";
+import React from 'react';
 
-function App() {
+const Hero = () => {
   return (
-    <div className="main-container">
-      <div className='container'>
-        <nav>
-            <div className='nav'>Home</div>
-            <div className='nav'>Works</div>
-            <div className='nav'>Contact</div>
-        </nav>
+    <>
+    <main id='home-sec' className='container mx-auto h-full flex items-center justify-between space-x-10 text-center'>
 
-        <div className='content'>
-            <img src={reactLogo} alt="" />
-             <div className='text'>
-                <h2>Welcome to my portfolio!</h2>
-                <p>I am a web developer and artist, blending creativity with technology to <br /> create engaging digital experiences. Explore my work and discover <br />the intersection of art and code.</p>
-            </div>
-
+      <div id='l-content' className='flex-col text-left space-y-5 text-[#D9D9D9] font-bold text-[23px] pl-20 pt-20'>
+        <h1 className="">Hi,</h1>
+        <h1 className=''>I'm <span className='text-[#D8871A]'>Pawpatrol</span></h1>
+        <p className='font-light text-lg'>Frontend Developer</p>
+        <div id='contact-btn' className='flex justify-left border-none pt-5 '>
+          <a href="#contact-sec"><button className='bg-transparent border-2 border-[#D8871A] text-[#D8871A] text-lg py-2 px-2 hover:border-[#D9D9D9] hover:text-[#D9D9D9]'>Contact Me</button></a>
         </div>
       </div>
-    </div>
-  );
-}
 
-export default App;
+      <div id='r-content' className='flex flex-row items-end justify-between h-full flex-1'>
+        <div id='profile' className='w-[35rem] h-[35rem] flex justify-center items-center '>
+          <img src="/public/profile.png" className='w-full h-full object-cover opacity-50' />
+        </div>
+
+        <div id='social' className='flex flex-col justify-center mr-20 items-center pb-20'>
+          
+          <div className="w-[4px] h-40 bg-[#D8871A] mb-4"></div>
+
+          <div className="flex flex-col space-y-4 text-2xl">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook text-[#D9D9D9] hover:text-[#D8871A]"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram text-[#D9D9D9] hover:text-[#D8871A]"></i>
+            </a>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github text-[#D9D9D9] hover:text-[#D8871A]"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter text-[#D9D9D9] hover:text-[#D8871A]"></i>
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+    </main>
+    </>
+  );
+};
+
+export default Hero;
